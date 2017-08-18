de3884887e86127eccb08fcfd67b06eb2dc63ec4
AurevoirXavier <c.estlavie@icloud.com>
2017-08-19 01:36:03 +0800

    1:35/8/19/17

---
+++ generics/src/main.rs
@@ -0,0 +1,25 @@
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let numbers = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&numbers);
+
+    println!("The largest number is {}", result);
+
+    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&numbers);
+
+    println!("The largest number is {}", result);
+}
