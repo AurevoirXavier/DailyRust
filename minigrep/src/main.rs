0ce1da86c7e696962877c54c235125021321f8e3
AurevoirXavier <c.estlavie@icloud.com>
2017-09-03 18:39:37 +0800

    18:39/9/3/17

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,14 +2,19 @@ extern crate minigrep;
 
 use std::env;
 use std::process;
+use std::io::Write;
 
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
 
-    let config = Config::new(&args).unwrap_or_else(|err|{
-        eprintln!("Problem parsing arguments: {}", err);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        writeln!(
+            &mut stderr,
+            "Problem parsing arguments: {}",
+            err
+        ).expect("Could not write to stderr");
 
         process::exit(1);
     });
