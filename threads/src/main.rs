bbdb86c13b9963d64da0e61209367033a338d403
AurevoirXavier <c.estlavie@icloud.com>
2017-09-04 17:25:08 +0800

    17:25/09/04/2017

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,3 +1,29 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    //    thread::spawn(|| {
+    //        for i in 1..10 {
+    //            println!("hi number {} from the spawned thread!", i);
+    //        }
+    //    });
+
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+    }
+
+    handle.join();
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join();
 }
