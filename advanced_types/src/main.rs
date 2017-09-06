745e8b6b4882cdfae82fbe09143a6117566ea930
AurevoirXavier <c.estlavie@icloud.com>
2017-09-06 12:38:05 +0800

    12:38/09/06/2017

--- advanced_types/src/main.rs
+++ advanced_types/src/main.rs
@@ -1,3 +1,31 @@
+type Kilometers = i32;
+
+type Thunk = Box<FnOnce() + Send + 'static>;
+
+use std::io::Error;
+use std::fmt;
+
+type Result_<T> = Result<T, Error>;
+
+pub trait Write {
+    fn write(&mut self, buf: &[u8]) -> Result_<usize>;
+
+    fn flush(&mut self) -> Result_<()>;
+
+    fn write_all(&mut self, buf: &[u8]) -> Result_<()>;
+
+    fn write_fmt(&mut self, fmt: fmt::Formatter) -> Result_<()>;
+}
+
+fn bar() -> ! {}
+
+fn generic<T: ? Sized>(t: &T) {}
+
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x + y);
+
+    let _f: Thunk = Box::new(|| println!("hi"));
 }
