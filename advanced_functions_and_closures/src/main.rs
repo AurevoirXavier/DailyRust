146ac2c84ee5625128c8b11ced9e7ecae9c2c67a
AurevoirXavier <c.estlavie@icloud.com>
2017-09-06 12:53:08 +0800

    12:53/09/06/2017

--- advanced_functions_and_closures/src/main.rs
+++ advanced_functions_and_closures/src/main.rs
@@ -1,3 +1,31 @@
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+fn _return_closure() -> Box<Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
+
+    let list_of_numbers = vec![1, 2, 3];
+
+    //    let list_of_strings: Vec<String> = list_of_numbers
+    //        .iter()
+    //        .map(|i| i.to_string())
+    //        .collect();
+
+    let list_of_strings: Vec<String> = list_of_numbers
+        .iter()
+        .map(ToString::to_string)
+        .collect();
+
+    println!("{:?}", list_of_strings);
 }
