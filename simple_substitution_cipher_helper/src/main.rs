feeed6cbca6ba633f896b6a3bb5d85bb0ef4a281
AurevoirXavier <c.estlavie@icloud.com>
2017-09-25 14:49:09 +0800

    14:49/09/25/2017

---
+++ simple_substitution_cipher_helper/src/main.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+fn main() {
+    let map1 = "abcdefghijklmnopqrstuvwxyz";
+    let map2 = "etaoinshrdlucmfwypvbgkjqxz";
+
+    let cipher = Cipher::new(map1, map2);
+
+    let encode = cipher.encode("abc");
+
+    println!("{}, {}", encode, cipher.decode(&encode));
+}
+
+struct Cipher {
+    map_encode: HashMap<char, char>,
+    map_decode: HashMap<char, char>
+}
+
+impl Cipher {
+    fn new(map1: &str, map2: &str) -> Cipher {
+        Cipher {
+            map_encode: map1.chars().zip(map2.chars()).collect::<HashMap<char, char>>(),
+            map_decode: map2.chars().zip(map1.chars()).collect::<HashMap<char, char>>()
+        }
+    }
+
+    fn encode(&self, string: &str) -> String {
+        string.chars().map(|c| self.map_encode.get(&c).unwrap()).collect()
+    }
+
+    fn decode(&self, string: &str) -> String {
+        string.chars().map(|c| self.map_decode.get(&c).unwrap()).collect()
+    }
+}
