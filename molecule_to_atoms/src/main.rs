3b1661b733a1d705fcd909768b9e7935104a548a
AurevoirXavier <c.estlavie@icloud.com>
2017-10-22 19:52:33 +0800

    19:52/10/22/2017

--- molecule_to_atoms/src/main.rs
+++ molecule_to_atoms/src/main.rs
@@ -2,8 +2,16 @@ extern crate molecule_to_atoms;
 
 use molecule_to_atoms::parse_molecule;
 
+/*
+    "Mg(OH)2"
+    => [("Mg",1),("O",2),("H",2)]
+
+    "K4[ON(SO3)2]2"
+    => [("K",4),("O",14),("N",2),("S",4)]
+*/
+
 fn main() {
-    let molecule = parse_molecule("K4[ON(SO3)2]2");
+    let molecule = parse_molecule("C6H12O6");
 
     println!("molecule: {:?}", molecule);
 }
