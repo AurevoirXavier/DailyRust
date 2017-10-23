691195d1f2e43880d863555cf64bbfe2a5ecdbd7
AurevoirXavier <c.estlavie@icloud.com>
2017-10-23 18:20:41 +0800

    18:20/10/23/2017

---
+++ valid_sudoku_with_size_nxn/src/main.rs
@@ -0,0 +1,72 @@
+extern crate valid_sudoku_with_size_nxn;
+
+use valid_sudoku_with_size_nxn::Sudoku;
+
+fn main() {
+    let good_sudoku_1 = Sudoku {
+        data: vec![
+            vec![7, 8, 4, 1, 5, 9, 3, 2, 6],
+            vec![5, 3, 9, 6, 7, 2, 8, 4, 1],
+            vec![6, 1, 2, 4, 3, 8, 7, 5, 9],
+            vec![9, 2, 8, 7, 1, 5, 4, 6, 3],
+            vec![3, 5, 7, 8, 4, 6, 1, 9, 2],
+            vec![4, 6, 1, 9, 2, 3, 5, 8, 7],
+            vec![8, 7, 6, 3, 9, 4, 2, 1, 5],
+            vec![2, 4, 3, 5, 6, 1, 9, 7, 8],
+            vec![1, 9, 5, 2, 8, 7, 6, 3, 4]
+        ]
+    };
+
+    let good_sudoku_2 = Sudoku {
+        data: vec![
+            vec![1, 4, 2, 3],
+            vec![3, 2, 4, 1],
+            vec![4, 1, 3, 2],
+            vec![2, 3, 1, 4],
+        ]
+    };
+
+    assert!(good_sudoku_1.is_valid());
+    assert!(good_sudoku_2.is_valid());
+
+    let bad_sudoku_1 = Sudoku {
+        data: vec![
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+        ]
+    };
+
+    let bad_sudoku_2 = Sudoku {
+        data: vec![
+            vec![1, 2, 3, 4, 5],
+            vec![1, 2, 3, 4],
+            vec![1, 2, 3, 4],
+            vec![1],
+        ]
+    };
+
+    let bad_sudoku_3 = Sudoku {
+        data: vec![
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+            vec![2, 3, 1, 5, 6, 4, 8, 9, 7],
+            vec![3, 1, 2, 6, 4, 5, 9, 7, 8],
+            vec![4, 5, 6, 7, 8, 9, 1, 2, 3],
+            vec![5, 6, 4, 8, 9, 7, 2, 3, 1],
+            vec![6, 4, 5, 9, 7, 8, 3, 1, 2],
+            vec![7, 8, 9, 1, 2, 3, 4, 5, 6],
+            vec![8, 9, 7, 2, 3, 1, 5, 6, 4],
+            vec![9, 7, 8, 3, 1, 2, 6, 4, 5]
+        ]
+    };
+
+    assert!(!bad_sudoku_1.is_valid());
+    assert!(!bad_sudoku_2.is_valid());
+    assert!(!bad_sudoku_3.is_valid());
+}
