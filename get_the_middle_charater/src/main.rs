81a861ce78eb03efbe727a08ce69c909efd99af3
AurevoirXavier <c.estlavie@icloud.com>
2017-11-24 23:45:17 +0800

    23:45/11/24/2017

--- get_the_middle_charater/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("{}", get_middle("test"));
-}
-
-fn get_middle(s: &str) -> &str {
-    let len = s.len();
-
-    if len % 2 == 0 { return &s[len / 2 - 1..len / 2 + 1]; } else { return &s[len / 2 - 1..len / 2]; }
-}
--- reversed_words/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("{}", reverse_words("hello world!"));
-}
-
-fn reverse_words(str: &str) -> String {
-    str.split_whitespace().rev().collect::<Vec<&str>>().join(" ")
-}
