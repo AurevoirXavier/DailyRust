41fc29a6ac586fc18b4475de61bc0d1596e07150
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-19 20:42:09 +0800

    20:42/04/19/18

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Formatter, Display};
+
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
     let (integer, boolean) = pair;
 
@@ -5,8 +7,31 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
 }
 
 #[derive(Debug)]
-struct Matrix(f32, f32, f32);
+struct Matrix(f32, f32, f32, f32);
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
+impl Matrix {
+    fn transpose(&self) -> Matrix {
+        Matrix(self.0, self.2, self.1, self.3)
+    }
+}
 
 fn main() {
+    let pair = (1, true);
 
+    println!("pair is {:?}", pair);
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+
+    println!("{}", matrix);
+
+    let transpose_matrix = matrix.transpose();
+
+    println!("{}", transpose_matrix);
 }
