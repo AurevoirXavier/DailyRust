60cfcc5cf2450e663cb735baa5e7447cf8bf86e5
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-19 21:08:40 +0800

    21:08/04/19/18

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -1,9 +1,15 @@
 use std::fmt::{self, Formatter, Display};
+use std::mem;
 
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
     let (integer, boolean) = pair;
 
     (boolean, integer)
+}
+
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
 }
 
 #[derive(Debug)]
@@ -23,15 +29,20 @@ impl Matrix {
 
 fn main() {
     let pair = (1, true);
-
     println!("pair is {:?}", pair);
     println!("the reversed pair is {:?}", reverse(pair));
 
     let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-
     println!("{}", matrix);
 
     let transpose_matrix = matrix.transpose();
-
     println!("{}", transpose_matrix);
+
+    let xs = [1, 2, 3, 4, 5];
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&xs[1 .. 4]);
 }
