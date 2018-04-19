2d4697e47b434f13fa2926e527d9e51a8f52b05e
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-19 22:06:00 +0800

    22:06/04/19/18

--- custom-types/src/main.rs
+++ custom-types/src/main.rs
@@ -1,4 +1,4 @@
-struct Nil;
+//struct Nil;
 
 struct Pair(i32, f32);
 
@@ -55,6 +55,51 @@ fn inspect(event: WebEvent) {
     }
 }
 
+#[allow(dead_code)]
+// enum with implicit discriminator (starts at 0)
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+#[allow(dead_code)]
+// enum with explicit discriminator
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
+use List::*;
+
+enum List {
+    Cons(u32, Box<List>),
+    Nil,
+}
+
+impl List {
+    fn new() -> List { Nil }
+
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => format!("{}, {}", head, tail.stringify()),
+            Nil => format!("Nil")
+        }
+    }
+}
+
 fn main() {
     let point = Point { x: 0.3, y: 0.4 };
     println!("point coordinates: ({}, {})", point.x, point.y);
@@ -66,7 +111,7 @@ fn main() {
         p2: Point { x: 0.5, y: 0.7 },
     };
 
-    let _nil = Nil;
+//    let _nil = Nil;
 
     let pair = Pair(1, 0.1);
 
@@ -91,4 +136,19 @@ fn main() {
     inspect(click);
     inspect(load);
     inspect(unload);
+
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
+
+    let mut list = List::new();
+
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
 }
