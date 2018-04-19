6f2853933d4ae5d5eae442befcd483ca0ef68236
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-19 20:34:52 +0800

    20:34/04/19/18

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -1,3 +1,12 @@
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+
+    (boolean, integer)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32);
+
 fn main() {
-    println!("Hello, world!");
+
 }
