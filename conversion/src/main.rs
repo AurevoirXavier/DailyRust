186cd11660788ac1cd9862d0ed5811d4e78b01f4
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-20 10:42:01 +0800

    10:42/04/20/18

--- conversion/src/main.rs
+++ conversion/src/main.rs
@@ -1,4 +1,7 @@
 use std::convert::From;
+use std::string::ToString;
+use std::str::FromStr;
+use std::num::ParseIntError;
 
 #[derive(Debug)]
 struct Number { value: i32 }
@@ -9,6 +12,25 @@ impl From<i32> for Number {
     }
 }
 
+#[derive(Debug)]
+struct Circle { radius: i32 }
+
+impl ToString for Circle {
+    fn to_string(&self) -> String {
+        format!("Circle of radius {:?}", self.radius)
+    }
+}
+
+impl FromStr for Circle {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
+        let radius = s.parse::<i32>()?;
+
+        Ok(Circle { radius })
+    }
+}
+
 fn main() {
     let num = Number::from(30);
     println!("My number is {:?}", num);
@@ -16,4 +38,16 @@ fn main() {
     let int = 5;
     let num: Number = int.into();
     println!("My number is {:?}", num);
+
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
+
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println! {"Sum: {}", sum};
+
+    println!("{:?}", Circle::from_str("5").unwrap());
+    println!("{:?}", "5".parse::<Circle>().unwrap());
 }
