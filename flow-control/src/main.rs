1590aa9213bb04cf3c42e6b63eca8e1ac69f3da7
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-21 20:47:57 +0800

    20:47/04/21/18

--- flow-control/src/main.rs
+++ flow-control/src/main.rs
@@ -1,3 +1,32 @@
+#[allow(unreachable_code)]
 fn main() {
-    println!("Hello, world!");
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            break 'outer;
+        }
+
+        println!("unreachable");
+    }
+
+    println!("Exited the outer loop");
+
+    let mut cnt = 0;
+    let result = loop {
+        cnt += 1;
+
+        if cnt == 10 { break cnt * 2; }
+    };
+
+    println!("{}", result);
+
+    let mut n = 1;
+
+    while n < 101 {
+        match n {}
+        n += 1;
+    }
 }
