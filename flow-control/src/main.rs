90f45e609cb8c67dc4d512bfbcccfbfc2ce404c9
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-22 12:04:25 +0800

    12:04/04/22/18

--- flow-control/src/main.rs
+++ flow-control/src/main.rs
@@ -26,7 +26,40 @@ fn main() {
     let mut n = 1;
 
     while n < 101 {
-        match n {}
+        let deal_3 = n % 3 == 0;
+
+        match n % 5 {
+            0 => if deal_3 { println!("fizzbuzz") } else { println!("buzz"); }
+            _ if deal_3 => println!("fizz"),
+            _ => println!("{}", n)
+        }
+
         n += 1;
     }
+
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter_mut() {
+        match name {
+            &mut "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
 }
