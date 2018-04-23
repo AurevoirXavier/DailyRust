deb0a88591436fe2fa42100a54ffa76ac62b032e
Aurevoir Xavier <c.estlavie@icloud.com>
2018-04-23 12:04:21 +0800

    12:04/04/23/18

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -148,11 +148,38 @@ fn main() {
     call_me(closure);
     call_me(function);
 
-    let fn_plain  = create_fn();
+    let fn_plain = create_fn();
     let mut fn_mut = create_fn_mut();
 //    let fn_once = create_fn_once();
 
     fn_plain();
     fn_mut();
 //    fn_once();
+
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+    println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    println!("2 in array1: {}", array1.iter().any(|&x| x == 2));
+    println!("2 in array2: {}", array2.into_iter().any(|&x| x == 2));
+
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
+
+    println!("Find 2 in vec1: {:?}", iter.find(|&&x| x == 2));
+    println!("Find 2 in vec2: {:?}", into_iter.find(|&x| x == 2));
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    println!("Find 2 in array1: {:?}", array1.iter().find(|&&x| x == 2));
+    println!("Find 2 in array2: {:?}", array2.into_iter().find(|&&x| x == 2));
 }
