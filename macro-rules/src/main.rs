5c202b5b7046036afe513fa879dc674be4bdd813
Xavier Lau <c.estlavie@icloud.com>
2018-05-19 11:31:22 +0800

    11:31/05/19/18

--- macro-rules/src/main.rs
+++ macro-rules/src/main.rs
@@ -14,6 +14,25 @@ macro_rules! multiply_add {
     ($a:expr, $b:expr, $c:expr) => {$a * ($b + $c)};
 }
 
+macro_rules! vec_strs {
+    (
+        $(
+            $element:expr
+        ),
+        *
+    ) => {
+        {
+            let mut v = Vec::new();
+
+            $(
+                v.push(format!("{}", $element));
+            )*
+
+            v
+        }
+    };
+}
+
 fn main() {
     let x = four!();
     println!("{}", x);
@@ -27,6 +46,7 @@ fn main() {
     let x = multiply_add!(2, 2, 3);
     println!("{}", x);
 
-
+    let x = vec_strs!(1, 2, 3, 4);
+    println!("{:?}", x);
 }
 
