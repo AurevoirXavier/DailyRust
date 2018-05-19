b95b8b982bd6e5eb148ab96ca863555a1f820ddc
Xavier Lau <c.estlavie@icloud.com>
2018-05-19 11:20:22 +0800

    11:20/05/19/18

--- macro-rules/src/main.rs
+++ macro-rules/src/main.rs
@@ -3,20 +3,28 @@ macro_rules! four {
 }
 
 macro_rules! gibberish {
-        (4 fn ['spang "whammo"] @_@) => {...};
-    }
+        (4 fn ['spang "whammo"] @_@) => { 1 };
+}
 
 macro_rules! multiply_add {
     ($a:expr, $b:expr, $c:expr) => {$a * ($b + $c)};
 }
 
+marco_rules! times_five {
+    ($e:expr) => {t * $e};
+}
+
 fn main() {
     let x = four!();
+    println!("{}", x);
 
+    let x = gibberish!(4 fn ['spang "whammo"] @_@);
     println!("{}", x);
 
     let x = multiply_add!(2, 2, 3);
+    println!("{}", x);
 
+    let x = times_five!();
     println!("{}", x);
 }
 
