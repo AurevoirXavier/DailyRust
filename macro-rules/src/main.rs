43fcb4f0af1a8f27f700cab87fff8a4489112ece
Xavier Lau <c.estlavie@icloud.com>
2018-06-03 23:25:19 +0800

    23:25/06/03/2018

--- macro-rules/src/main.rs
+++ macro-rules/src/main.rs
@@ -443,6 +443,28 @@ macro_rules! parse_unitary_variants {
     };
 }
 
+macro_rules! bct {
+    // cmd 0:  d ... => ...
+    (0, $($ps:tt),* ; $_d:tt)
+        => (bct!($($ps),*, 0 ; ));
+    (0, $($ps:tt),* ; $_d:tt, $($ds:tt),*)
+        => (bct!($($ps),*, 0 ; $($ds),*));
+
+    // cmd 1p:  1 ... => 1 ... p
+    (1, $p:tt, $($ps:tt),* ; 1)
+        => (bct!($($ps),*, 1, $p ; 1, $p));
+    (1, $p:tt, $($ps:tt),* ; 1, $($ds:tt),*)
+        => (bct!($($ps),*, 1, $p ; 1, $($ds),*, $p));
+
+    // cmd 1p:  0 ... => 0 ...
+    (1, $p:tt, $($ps:tt),* ; $($ds:tt),*)
+        => (bct!($($ps),*, 1, $p ; $($ds),*));
+
+    // Halt on empty data string:
+    ( $($ps:tt),* ; )
+        => (());
+}
+
 fn main() {
     let x = four!();
     println!("{}", x);
