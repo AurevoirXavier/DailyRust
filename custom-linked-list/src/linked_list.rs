c74f599f483ba11357c2c0fbac8043ca9bd992e8
Xavier Lau <c.estlavie@icloud.com>
2018-09-22 21:12:19 +0800

    21:12/09/22/18

--- custom-linked-list/src/linked_list.rs
+++ custom-linked-list/src/linked_list.rs
@@ -2,21 +2,21 @@ use std::mem;
 use std::ops::{Index, IndexMut};
 
 #[derive(Debug)]
-pub enum LinkedList<T> {
+pub enum LinkedListEnum<T> {
     Node {
         val: T,
-        next: Box<LinkedList<T>>,
+        next: Box<LinkedListEnum<T>>,
     },
     Empty,
 }
 
-impl<T> Index<usize> for LinkedList<T> {
-    type Output = LinkedList<T>;
+impl<T> Index<usize> for LinkedListEnum<T> {
+    type Output = LinkedListEnum<T>;
 
-    fn index(&self, index: usize) -> &LinkedList<T> {
+    fn index(&self, index: usize) -> &LinkedListEnum<T> {
         match index {
             0 => self,
-            _ => if let LinkedList::Node {
+            _ => if let LinkedListEnum::Node {
                 val: _,
                 next,
             } = self { next.index(index - 1) } else { panic!() }
@@ -24,11 +24,11 @@ impl<T> Index<usize> for LinkedList<T> {
     }
 }
 
-impl<T> IndexMut<usize> for LinkedList<T> {
-    fn index_mut(&mut self, index: usize) -> &mut LinkedList<T> {
+impl<T> IndexMut<usize> for LinkedListEnum<T> {
+    fn index_mut(&mut self, index: usize) -> &mut LinkedListEnum<T> {
         match index {
             0 => self,
-            _ => if let LinkedList::Node {
+            _ => if let LinkedListEnum::Node {
                 val: _,
                 next,
             } = self { next.index_mut(index - 1) } else { panic!() }
@@ -36,55 +36,100 @@ impl<T> IndexMut<usize> for LinkedList<T> {
     }
 }
 
-impl<T> LinkedList<T> {
-    pub fn new() -> LinkedList<T> { LinkedList::Empty }
-
-    fn get_self(&mut self) -> LinkedList<T> { mem::replace(self, LinkedList::Empty) }
-
-    fn len(&self) -> usize {
-        let mut linked_list = self;
-        let mut len = 0;
-        loop {
-            if let LinkedList::Node {
-                val: _,
-                next,
-            } = linked_list {
-                linked_list = next;
-                len += 1;
-            } else { return len; }
-        }
-    }
+impl<T> LinkedListEnum<T> {
+    pub fn new() -> LinkedListEnum<T> { LinkedListEnum::Empty }
 
     pub fn insert(&mut self, index: usize, ins_val: T) {
         if index == 0 {
-            let ins_next = self.get_self();
-            mem::replace(self, LinkedList::Node {
+            let ins_next = mem::replace(self, LinkedListEnum::Empty);
+            mem::replace(self, LinkedListEnum::Node {
                 val: ins_val,
                 next: Box::new(ins_next),
             });
         } else {
-            if let LinkedList::Node {
+            if let LinkedListEnum::Node {
                 val: _,
                 ref mut next,
             } = self[index - 1] {
-                let ins_next = mem::replace(next, Box::new(LinkedList::Empty));
-                *next = Box::new(LinkedList::Node {
+                *next = Box::new(LinkedListEnum::Node {
                     val: ins_val,
-                    next: ins_next,
+                    next: mem::replace(next, Box::new(LinkedListEnum::Empty)),
                 });
             }
         }
     }
 
     pub fn remove(&mut self, index: usize) {
-        let len = self.len();
-        if index == len { self[len - 1] = LinkedList::Empty; } else {
+        let prev = &mut self[index - 1];
+        let ins_next = mem::replace(&mut prev[2], LinkedListEnum::Empty);
+        if let LinkedListEnum::Node {
+            val: _,
+            ref mut next,
+        } = prev { *next = Box::new(ins_next); }
+    }
+}
+
+#[derive(Debug)]
+pub struct LinkedListStruct<T> {
+    val: Option<T>,
+    next: Option<Box<LinkedListStruct<T>>>,
+}
+
+impl<T> Index<usize> for LinkedListStruct<T> {
+    type Output = LinkedListStruct<T>;
+
+    fn index(&self, index: usize) -> &LinkedListStruct<T> {
+        let mut find = self;
+        for _ in 0..index {
+            if let Some(ref next) = find.next {
+                find = next;
+            } else { panic!() }
+        }
+
+        find
+    }
+}
+
+impl<T> IndexMut<usize> for LinkedListStruct<T> {
+    fn index_mut(&mut self, index: usize) -> &mut LinkedListStruct<T> {
+        let mut find = self;
+        for _ in 0..index {
+            if let Some(ref mut next) = find.next {
+                find = next;
+            } else { panic!() }
+        }
+
+        find
+    }
+}
+
+impl<T> LinkedListStruct<T> {
+    pub fn new() -> LinkedListStruct<T> {
+        LinkedListStruct {
+            val: None,
+            next: None,
+        }
+    }
+
+    pub fn insert(&mut self, index: usize, ins_val: T) {
+        if index == 0 { self.val = Some(ins_val); } else {
             let prev = &mut self[index - 1];
-            let ins_next = prev[2].get_self();
-            if let LinkedList::Node {
-                val: _,
-                ref mut next,
-            } = prev { *next = Box::new(ins_next); }
+            prev.next = Some(Box::new(LinkedListStruct {
+                val: Some(ins_val),
+                next: mem::replace(&mut prev.next, None),
+            }));
+        }
+    }
+
+    pub fn remove(&mut self, index: usize) {
+        if index == 0 {
+            if let Some(ref mut next) = self.next {
+                self.val = mem::replace(&mut next.val, None);
+                self.next = mem::replace(&mut next.next, None);
+            } else { self.val = None; }
+        } else {
+            let prev = &mut self[index - 1];
+            prev.next = mem::replace(&mut prev[1].next, None);
         }
     }
 }
--- custom-linked-list/src/main.rs
+++ custom-linked-list/src/main.rs
@@ -1,17 +1,32 @@
 mod linked_list;
 
-use self::linked_list::LinkedList;
+use self::linked_list::*;
 
 fn main() {
-    let mut linked_list = LinkedList::new();
+    println!("{:-^1$}", "Enum Version", 100);
 
-    linked_list.insert(0, 1);
-    linked_list.insert(1, 3);
-    linked_list.insert(2, 5);
-//
-    println!("{:#?}", linked_list);
+    let mut linked_list_enum = LinkedListEnum::new();
 
-    linked_list.remove(1);
+    linked_list_enum.insert(0, 1);
+    linked_list_enum.insert(1, 3);
+    linked_list_enum.insert(1, 5);
 
-    println!("\ndelete from index 1...\n\n{:#?}", linked_list);
+    println!("{:#?}", linked_list_enum);
+
+    linked_list_enum.remove(1);
+
+    println!("\ndelete from index 1...\n\n{:#?}", linked_list_enum);
+    println!("{:-^1$}", "Struct Version", 100);
+
+    let mut linked_list_struct = LinkedListStruct::new();
+
+    linked_list_struct.insert(1, 1);
+    linked_list_struct.insert(1, 3);
+    linked_list_struct.insert(0, 5);
+
+    println!("{:#?}", linked_list_struct);
+
+    linked_list_struct.remove(0);
+
+    println!("\ndelete from index 0...\n\n{:#?}", linked_list_struct);
 }
